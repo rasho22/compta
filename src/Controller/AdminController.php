142cc195a7ab2884643ba9e1d4b7d43ec9adc6af
stephane-legalliard <le-galliard.stephane@orange.fr>
2016-10-21 13:39:20 +0200

    create function user in controller file

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,61 +11,37 @@ use compta\Domain\Depenses;
 
 class AdminController {
 
-    public function loginAction(Request $request, Application $app) {
-        return $app->render('/login', array(
-            'error'         => $app['security.last_error']($request),
-            'last_username' => $app['session']->get('_security.last_username'),
-        ));
-    }
+    
 
     /**
-     * Add group controller.
-    
-     */
-    public function addGroupAction(Request $request, Application $app) {
-        $group = new UserGroup();
-        $group->setGroup($request->request->get('id_user_group'));
-        $group->setGroupName($request->request->get('group_name'));
-        
-        if (!$request->request->has('id_user_group')) {
-            return $app->json('Missing required parameter: id_user_group', 400);
-        }
-        if (!$request->request->has('group_name')) {
-            return $app->json('Missing required parameter: group_name', 400);
-        }
-        $responseData = $this->buildGroupArray($group);
-         return $app->json($responseData, 201);  // 201 = Created
-
-         $app['dao.group']->save($group);
-    }
-
-     /**
-     * API delete group controller.
+     * Add user controller.
      *
-     * @param integer $id group id
+     * @param Request $request Incoming request
      * @param Application $app Silex application
      */
-    public function editGroupAction($id_user_group, Application $app) {
-        // Delete all associated depenses
-        $app['dao.depense']->deleteAllByGroup($id_user_group);
-        // Delete the group
-        $app['dao.group']->delete($id_user_group);
-        return $app->json('No Content', 204);  // 204 = No content
+    public function addUserAction(Request $request, Application $app) {
+        $user = new User();
+              -> setName($user_name)
+              -> setColor($color)
+              -> setGroup($user_group)
+        $app['dao.user']->save($user);
+        return $app->json;
     }
 
+    
     /**
-     * Converts an group object into an associative array for JSON encoding
+     * Delete user controller.
      *
-     *
-     *
-     * @return array Associative array whose fields are the group properties.
+     * @param integer $id User id
+     * @param Application $app Silex application
      */
-    private function buildGroupArray(Group $group) {
-        $data  = array(
-            'id_user_group' => $group->getId(),
-            'group_name' => $group->getName(),
-            );
-        return $data;
-    }
-
-  } 
+    public function deleteUserAction($id, Application $app) {
+        $app['dao.user']->delete($id);
+        $app['dao.usergroup']->deleteByGroup($id);
+        $app['dao.depenses']->deleteByUser($id);
+        return $app->json(array(
+            'status' => 'OK'
+                ), 200);
+        }
+        
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,33 +4,26 @@ namespace compta\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
-use compta\Domain\UserGroup;
-use compta\Domain\User;
 use compta\Domain\Depenses;
+use compta\Domain\User;
+use compta\Domain\UserGroup;
+
+
 
 class HomeController {
 
     /**
-     * group details controller.
+     * Home page controller.
      *
-     * @param integer $id group id
-     * @param Request $request Incoming request
      * @param Application $app Silex application
      */
-    public function groupAction($id_user_group, Request $request, Application $app) {
-        $group = $app['dao.group']->find($id_user_group);
-        
-        if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // A user is fully authenticated : he can add comments
-            $depense = new Depense();
-            $depense->setGroup($group);
-            $user = $app['user'];
-            $depense->setAuthor($user);
-           
-        }
-        $depenses = $app['dao.depense']->findAllByGroup($id);
-         return $app->json((array('success', 'Details the group.'));
-    }
+    public function indexAction(Application $app) {
+        $user = $app['dao.user']->findAll()
+        return $app->json(array(
+            'records' => $result,
+            'status' => 'OK'
+        ), 200);
 
-   
+    
+    }
 }
