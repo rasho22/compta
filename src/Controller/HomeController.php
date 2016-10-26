13978715804553cf99e0ee53f57dc3871d63f9a2
foch01 <tsanceo@yahoo.com>
2016-10-26 16:29:43 +0200

    add folder web

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,18 +12,16 @@ use compta\Domain\Depenses;
 class HomeController {
 
     /**
-
      * Home page controller.
      *
      * @param Application $app Silex application
      */
     public function indexAction(Application $app) {
-          $user = $app['dao.user']->findAll()
+          $user = $app['dao.user']->findAll();
         return $app->json(array(
             'records' => $result,
             'status' => 'OK'
         ), 200);
-
     }
 
     public function groupAction($id_user_group, Request $request, Application $app) {
