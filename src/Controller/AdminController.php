202473186f5d28ab119baf73358c1cf03c9af43b
Valentin <valentinsusset@hotmail.com>
2016-10-26 14:46:16 +0200

    read, edit et delete depense dans admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,29 +10,17 @@ use compta\Domain\Depenses;
 
 class AdminController {
 
+
     public function loginAction(Request $request, Application $app) {
-        $app['security.firewalls'] = array(
-            'admin' => array(
-                'pattern' => '^/admin/',
-                'form' => array('login_path' => '/login', 'check_path' => '/admin/login_check'),
-            ),
-        );
+        return $app->render('/login', array(
+            'error'         => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+        ));
     }
 
-    public function logoutAction (Request $request, Application $app)
-    {
-        $app['security.firewalls'] = array(
-            'secured' => array(
-                'pattern' => '^/admin/',
-                'form' => array('login_path' => '/login', 'check_path' => '/admin/login_check'),
-                'logout' => array('logout_path' => '/admin/logout', 'invalidate_session' => true),
-            ),
-        );
-    }
-        /**
-     * Add user controller.
     /**
      * Admin home page controller.
+
      *
      * @param Request $request Incoming request
      * @param Application $app Silex application
@@ -149,7 +137,12 @@ class AdminController {
 
     }
 
-
+    //read depense
+    public function getDepenseAction($id, Application $app) {
+        $depense = $app["dao.depense"]->findById($id);
+        $responseData = $this->buildDepenseArray($depense);
+        return $app->json($responseData);
+    }
 
 
     /**
@@ -160,14 +153,14 @@ class AdminController {
      * @param Application $app Silex application
      */
     public function editDepenseAction($id, Request $request, Application $app) {
-        $depense = $app['dao.depense']->find($id);
-        if ($depenseForm->isSubmitted() && $depenseForm->isValid()) {
+        $depense = $app['dao.depense']->findById($id);
+        if ($request->request->has('montant') AND $request->request->has('date') AND $request->request->has('description') AND $request->request->has('id_users')) {
             $app['dao.depense']->save($depense);
-            $app['session']->getFlashBag()->add('success', 'The depense was succesfully updated.');
         }
-        /*return $app['twig']->render('comment_form.html.twig', array(
-            'title' => 'Edit comment',
-            'commentForm' => $commentForm->createView()));*/
+
+        else {
+            echo "Il manque des paramètres !";
+        }
     }
 
     /**
@@ -178,7 +171,6 @@ class AdminController {
      */
     public function deleteDepenseAction($id, Application $app) {
         $app['dao.depense']->delete($id);
-        $app['session']->getFlashBag()->add('success', 'The depense was succesfully removed.');
         // Redirect to admin home page
         return $app->redirect($app['url_generator']->generate('admin'));
     }
