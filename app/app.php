1a9b4a2118c0c6e3197d3efb3f03e6841d613b2a
foch01 <tsanceo@yahoo.com>
2016-11-02 14:23:28 +0100

    add function login

--- app/app.php
+++ app/app.php
@@ -9,7 +9,26 @@ ExceptionHandler::register();
 // Register service providers
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+     'security.firewalls' => array(
+         'secured' => array(
+             'pattern' => '^/',
+             'anonymous' => true,
+             'logout' => true,
+             'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+             'users' => $app->share(function () use ($app) {
+                 return new compta\DAO\UserDAO($app['db']);
+             }),
+         ),
+     ),
+     'security.role_hierarchy' => array(
+         'ROLE_ADMIN' => array('ROLE_USER'),
+     ),
+     'security.access_rules' => array(
+         array('^/admin', 'ROLE_ADMIN'),
+     ),
+ ));
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/app_compta.log',
