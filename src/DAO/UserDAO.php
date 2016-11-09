82d663bb81aa00b576e4b4fdce1cafe056b0ce9a
Valentin <valentinsusset@hotmail.com>
2016-11-09 15:16:28 +0100

    enlevé findAll doublon dans userDAO

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -96,25 +96,6 @@ class UserDAO extends DAO
 
 
 
-   /**
-    * Return a list of all users, sorted by date .
-    *
-    * @return array A list of all users.
-    */
-   
-   public function findAll() {
-       $sql = "select * from users";        
-       $result = $this->getDb()->fetchAll($sql);
-       
-       // Convert query result to an array of domain objects
-       $users = array();
-       foreach ($result as $row) {
-           $usersId = $row['id_users'];
-           $users[$usersId] = $this->buildDomainObject($row);
-       }
-       return $users;
-   }   
-
     /**     * Creates a User object based on a DB row.     *     * @param array $row The DB row containing Users data.     * @return compta\Domain\User     */   
 
     protected function buildDomainObject($row) {        
